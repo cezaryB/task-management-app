c0371d61a9b8b394f7af8ac5de5af0e2a60d1050
Cezary <czarekburzykowski@gmail.com>
2019-10-26 17:27:51 +0200

    Working on auth module

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Post, ValidationPipe, Body } from '@nestjs/common';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('/signup')
+  async signUp(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    this.authService.signUp(authCredentialsDto);
+  }
+}
