9758a313e5f264cf6241887b59617eee3f341ac7
Cezary <czarekburzykowski@gmail.com>
2019-11-03 19:02:23 +0100

    Small change

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -5,7 +5,6 @@ import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 import { Task } from './task.entity';
-import { DeleteResult, UpdateResult } from 'typeorm';
 import { AuthGuard } from '@nestjs/passport';
 
 @Controller('tasks')
